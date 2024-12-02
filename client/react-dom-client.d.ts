e909b314595d22422901c27772e4406068bee9a6
Jean Yves Hector <jeanyveshector@gmail.com>
2024-12-01 21:26:10 -0600

    new Commierrt

--- client/react-dom-client.d.ts
+++ client/react-dom-client.d.ts
@@ -2,4 +2,4 @@ declare module 'react-dom/client' {
     import * as ReactDOM from 'react-dom';
     export function createRoot(container: Element | DocumentFragment): ReactDOM.Root;
   }
-  
+  //notice this change
